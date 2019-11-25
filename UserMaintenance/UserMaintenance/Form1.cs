4911385d83769a393a4b08977b83b8b4735f6685
Ádám Hunor Fülöp <adam.fulop2@stud.uni-corvinus.hu>
2019-11-25 23:28:56 +0100

    LastName -> Fullname

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -18,7 +18,7 @@ namespace UserMaintenance
         {
             InitializeComponent();
             label1.Text = Resource1.Lastname;
-            label2.Text = Resource1.FirstName;
+            
             button1.Text = Resource1.Add;
             listBox1.DataSource = users;
             listBox1.DisplayMember = "Fullname";
@@ -29,8 +29,7 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text            
 
             };
             users.Add(u);
